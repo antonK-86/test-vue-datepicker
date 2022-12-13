b99c8e3072084fcc11a2e0c5758ea95bc1b8fa87
antonK-86 <ank86@inbox.ru>
2022-12-13 23:41:03 +0300

    test

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/test-vue-datepicker/dist",
+  publicPath: "/test-vue-datepicker/",
 });
